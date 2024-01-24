5cfe1251f45fce8c7a30b5bcfc367dffae760c22
Danny <danny.dauck@gmail.com>
2024-01-24 17:39:42 +0100

    Initial Commit

---
+++ SwiftCaterpie/SwiftCaterpieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCaterpieApp.swift
+//  SwiftCaterpie
+//
+//  Created by Danny Dauck on 24.01.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCaterpieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
